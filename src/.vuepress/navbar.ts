56e680ce33dc84e1c2a27a93e9449f2db7d43aa1
yuanmoc <gyuanmoc@gmail.com>
2025-04-25 14:15:03 +0800

    添加 icon

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,13 +2,13 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  { text: "文章", link: "/article/" },
-  { text: "分类", link: "/category/" },
-  { text: "标签", link: "/tag/" },
-  { text: "归档", link: "/timeline/" },
+  { text: "文章", link: "/article/" , icon: "material-symbols:post-rounded"},
+  { text: "分类", link: "/category/", icon: "tabler:category-filled" },
+  { text: "标签", link: "/tag/", icon: "material-symbols:tag-rounded" },
+  { text: "归档", link: "/timeline/", icon: "gravity-ui:list-timeline" },
   {
     text: "文档",
-    icon: "pen-to-square",
+    icon: "f7:doc-on-doc-fill",
     prefix: "/docs/",
     children: [
     //   {
@@ -22,7 +22,7 @@ export default navbar([
     //       "4",
     //     ],
     //   },
-      { text: "介绍", icon: "pen-to-square", link: "intro" },
+      { text: "介绍", icon: "f7:doc-fill", link: "intro" },
     ],
   },
 
