a79eff566cf4beec11cbc87d65d1740b5b4cd6d4
yuanmoc <gyuanmoc@gmail.com>
2025-05-01 09:14:14 +0800

    modify-frontmatter-date-plugin

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,9 @@
-import { defineUserConfig } from "vuepress";
+import {defineUserConfig} from "vuepress";
 
 import theme from "./theme.js";
 import setFrontmatter from "./util/setFrontmatter.js"
 import dailyListPlugin from "./util/dailyListPlugin.js"
-import { baiduAnalyticsPlugin } from '@vuepress/plugin-baidu-analytics'
+import {baiduAnalyticsPlugin} from '@vuepress/plugin-baidu-analytics'
 
 export default defineUserConfig({
   base: "/",
@@ -25,5 +25,23 @@ export default defineUserConfig({
     baiduAnalyticsPlugin({
       id: "1a2e8709a79bd587207c1e48121859d7"
     }),
+    {
+      // 时间默认转 UTC 时间，实际多了8个小时，现在进行减少
+      name: 'modify-frontmatter-date-plugin',
+      onInitialized: (app) => {
+        app.pages.forEach((page) => {
+          // 示例：统一修改最后更新时间
+          const date = page.frontmatter.date
+          if (date) {
+            // 将字符串转换为 Date 对象
+            const originalDate = new Date(date);
+            // 直接在 UTC 时间上减小时
+            originalDate.setUTCHours(originalDate.getUTCHours() - 8);
+            // 转换为 UTC 字符串
+            page.frontmatter.date = originalDate.toISOString();
+          }
+        })
+      }
+    }
   ],
 });
